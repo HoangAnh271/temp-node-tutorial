53434f24da4ec55911bbc621f23620f0c4815a2d
unknown <hoangnora271@gmail.com>
2023-05-15 19:29:57 +0700

    first commit

---
+++ 2-globals-variable.js
@@ -0,0 +1,15 @@
+// GLOBALS VARIABLE - NO WINDOW!!!!
+// __dirname - path to current directory
+// __filename - file name
+// require  - function to use modules (CommonJS)
+// module - info about cureent module (file)
+// process - info about env where the program is being executed
+
+// __dirname
+console.log(__dirname);
+
+setInterval(() => {
+  console.log("hello world");
+}, 1000);
+// __filename
+console.log(__filename);
